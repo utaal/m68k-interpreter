24704372d02b64c0fbc771dbc2f0615ecc415b31
Andrea Lattuada <utaaal@gmail.com>
2012-03-26 19:09:08 +0200

    CPUState class

--- src/main/scala/CPUState.scala
+++ src/main/scala/CPUState.scala
@@ -17,12 +17,51 @@ object bitwise {
 
 import bitwise._
 
-case class RegisterState(val value: Long) {
+case class RegisterState(value: Long) {
   require (value >= 0L && value <= 0xffffffffL)
   def get(size: Size): Long = size mask value
   def set(size: Size, newValue: Long) =
-    new RegisterState(value - (size mask value) + (size mask newValue))
+    new RegisterState((size reverseMask value) | (size mask newValue))
 }
 
-class CPUState {
-} 
+case class ProgramCounter(value: Long) {
+  require (value >= 0L && value <= 0xffffffL)
+}
+
+case class StatusRegister(C: Boolean, V: Boolean, Z: Boolean, N: Boolean, X: Boolean)
+
+object CPUState {
+  val DataRegisterNumber = 8
+  val AddressRegisterNumber = 8
+  val FramePointerRegister = 6
+  val StackPointerRegister = 7
+}
+
+case class CPUState(
+  D: Vector[RegisterState],
+  A: Vector[RegisterState],
+  PC: ProgramCounter,
+  SR: StatusRegister
+) {
+  val dataRegisters = (0 to 8) map (RegisterState(_))
+  
+  private def updatedRegVector(vec: Vector[RegisterState], num: Int, size: Size, value: Long) =
+    vec.updated(num, vec(num).set(size, value))
+
+  def setD(number: Int, size: Size, value: Long) = {
+    require (number >= 0 && number < CPUState.DataRegisterNumber)
+    this.copy(D = updatedRegVector(D, number, size, value))
+  }
+
+  def setA(number: Int, size: Size, value: Long) = {
+    require (number >= 0 && number < CPUState.AddressRegisterNumber)
+    this.copy(A = updatedRegVector(A, number, size, value))
+  }
+
+  def setFP(size: Size, value: Long) =
+    setA(CPUState.FramePointerRegister, size, value)
+
+  def setSP(size: Size, value: Long) =
+    setA(CPUState.StackPointerRegister, size, value)
+}
+
